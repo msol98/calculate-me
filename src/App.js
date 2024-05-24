435423da7fcd008ab42d73d4fd08ea2552b8b289
MSoleimanzadeh <mina.soleimanzadeh@gmail.com>
2024-05-24 23:48:43 +0330

    Add calculation functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  var operators = ['+', '-', '*', '/'];
+  var digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const [num, setNum] = useState();
+  const [result, setResult] = useState();
+  const [operations, setOperations] = useState('');
+
+  function updateNumber(oldNumber, newDigit) {
+    return (oldNumber | 0) * 10 + newDigit;
+  }
+
+  function insertDigit(input) {
+    setNum(updateNumber(num, input));
+  }
+
+  function insertOperand(input) {
+    const oldO = operations;
+    setOperations(oldO + num + input);
+    setNum(null);
+  }
+
+  function handleCalculation() {
+    const oldOperations = operations;
+    setResult(eval(oldOperations + num));
+    setOperations(null);
+    setNum(null);
+  }
+
   return (
     <div>
-      
+      <div>
+        {operators.map(operand => <button onClick={() => insertOperand(operand)} key={operand}>{operand}</button>)}
+        <button onClick={handleCalculation}>=</button>
+      </div>
+      <br />
+      <div>
+        {digits.map(digit => <button onClick={() => insertDigit(digit)} key={digit}>{digit}</button>)}
+      </div>
     </div>
   );
 }
